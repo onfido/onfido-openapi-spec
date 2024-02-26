0a25c96307bace098ff86bb9e53b330cdb024367
Davide Vacca <davide.vacca@onfido.com>
2024-02-26 15:39:33 +0100

    Fix env

--- generator/script/generate.sh
+++ generator/script/generate.sh
@@ -7,9 +7,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASEDIR=${SCRIPT_DIR}/../..
 
 CONFIG_FILES=${*:-`ls generator/configuration/*.yaml`}
+GENERATED_CONFIG_FILES=""
 
-OPENAPI_GENERATOR_COMMAND=${OPENAPI_GENERATOR:-docker run \
-  --rm -v "$(pwd):/local" -w /local \
+OPENAPI_GENERATOR_COMMAND=${OPENAPI_GENERATOR_COMMAND:-\
+  docker run --rm -v "$(pwd):/local" -w /local \
   openapitools/openapi-generator-cli:v7.3.0}
 
 cd ${BASEDIR}
@@ -19,14 +20,23 @@ for CONFIG_FILE in $CONFIG_FILES
 do
   GENERATOR=$(basename $CONFIG_FILE .yaml)
 
-  if [ "${GENERATOR}" != "common" ]; then
+  if [ "${GENERATOR}" != "common" ];
+  then
+    GENERATED_CONFIG_FILE=generated/configuration/${GENERATOR}.yaml
+
     ( cat generator/configuration/common.yaml && echo &&
       cat generator/configuration/${GENERATOR}.yaml ) | \
         API_VERSION=1.6.0 GENERATOR=${GENERATOR} envsubst >| \
-        generated/configuration/${GENERATOR}.yaml
+        ${GENERATED_CONFIG_FILE}
 
     echo "Configuration for generator ${GENERATOR} built."
+
+    GENERATED_CONFIG_FILES="${GENERATED_CONFIG_FILES} ${GENERATED_CONFIG_FILE}"
   fi
 done
 
-${OPENAPI_GENERATOR_COMMAND} batch --clean --root-dir /local generated/configuration/*.yaml
+pwd
+ls *
+head ./schemas/workflow_runs/workflow_run_builder.yaml
+
+${OPENAPI_GENERATOR_COMMAND} batch --clean ${GENERATED_CONFIG_FILES}
