3ff16e51e1b2cba9752dd9be2a1c4e3ce8e6a94f
Davide Vacca <davide.vacca@onfido.com>
2024-04-18 11:11:14 +0200

    Add Python configuration

---
+++ generators/python/urllib3/templates/webhook_event_verifier.py
@@ -0,0 +1,24 @@
+import hmac
+import hashlib
+import json
+
+from .models import WebhookEvent
+
+class OnfidoInvalidSignatureError(Exception):
+    pass
+
+class WebhookEventVerifier:
+    def __init__(self, webhook_token):
+        self.webhook_token = webhook_token
+
+    def read_payload(self, raw_event, signature):
+        # Compute the the actual HMAC signature from the raw request body.
+        event_signature = hmac.new(key=self.webhook_token.encode("utf-8"),
+                                   msg=raw_event.encode("utf-8"),
+                                   digestmod=hashlib.sha256).hexdigest()
+
+        # Compare the signatures (prevent against timing attacks).
+        if not hmac.compare_digest(signature, event_signature):
+            raise OnfidoInvalidSignatureError("Invalid signature for webhook event")
+
+        return WebhookEvent.from_json(raw_event)
