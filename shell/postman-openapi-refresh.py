892c889bb9fbdc8ad7935a893fe56513831f3e65
Davide Vacca <davide.vacca@onfido.com>
2024-04-29 17:51:38 +0200

    Update postman refresh script and spec

--- shell/postman-openapi-refresh.py
+++ shell/postman-openapi-refresh.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import json
 import logging
+import requests
 import sys
 
 from dataclasses import dataclass
+from time import sleep
+from os import getenv
 
 
 @dataclass
@@ -41,15 +44,28 @@ SECTIONS = (
 )
 
 
+def get_logger() -> logging.Logger:
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+    return root
+
+
 def convert_path(snake_str: str) -> None:
     return " ".join(x.capitalize() for x in snake_str.lower().split("_"))
 
 
-def convert_spec(input_spec_file: str, output_spec_file: str) -> None:
+def patch_spec(input_spec_file: str, output_spec_file: str) -> dict:
     with open(input_spec_file) as input_handler:
-        input_dict = json.load(input_handler)
+        spec_dict = json.load(input_handler)
 
-        for path in input_dict['paths'].keys():
+        for path in spec_dict['paths'].keys():
             path_prefix, resource_name = path.split('/')[1], None
 
             for section in SECTIONS:
@@ -61,14 +77,82 @@ def convert_spec(input_spec_file: str, output_spec_file: str) -> None:
                     break
 
             if not resource_name:
-                logging.warning('Skipping path prefix: {}'.format(path_prefix))
+                logger.warning('Skipping path prefix: {}'.format(path_prefix))
                 continue
 
-            for method in input_dict['paths'][path]:
-                input_dict['paths'][path][method]['tags'] = [tag]
+            for method in spec_dict['paths'][path]:
+                spec_dict['paths'][path][method]['tags'] = [tag]
 
     with open(output_spec_file, 'w') as fp:
-        json.dump(input_dict, fp, indent=2)
+        json.dump(spec_dict, fp, indent=2)
+
+    return spec_dict
+
+
+def update_postman_collection(spec_dict: dict, api_id: str,
+                              schema_id: str, file_path: str,
+                              collection_id: str = None) -> None:
+
+    postman_base = f"https://api.getpostman.com/apis/{api_id}"
+
+    headers = {'Accept': 'application/vnd.api.v10+json',
+               'Content-Type': "application/json",
+               'X-API-Key': getenv('POSTMAN_API_KEY')}
+
+    # Update OpenAPI schema
+    # https://www.postman.com/postman/workspace/postman-public-workspace/request/12959542-7ad78218-f1c6-48a6-b612-3cf64cdde475
+    result = requests.put(
+                url=f"{postman_base}/schemas/{schema_id}/files/{file_path}",
+                headers=headers,
+                data=json.dumps({
+                    "content": json.dumps(spec_dict, indent=2)
+                }))
+
+    result.raise_for_status()
+
+    logger.info("API updated")
+
+    if not collection_id:
+        # Add collection if collection id wasn't provided
+        # https://www.postman.com/postman/workspace/postman-public-workspace/request/12959542-c70373b3-b7df-4f7e-90a1-f0d0f2fe9370
+        result = requests.post(url=f"{postman_base}/collections",
+                               headers=headers,
+                               data=json.dumps({
+                                  "operationType": "GENERATE_FROM_SCHEMA",
+                                  "options": {
+                                    "folderStrategy": "Tags"
+                                    }
+                                  }))
+
+        result.raise_for_status()
+
+        logger.info("Collection created")
+    else:
+        # Or just sync collection, need to Update Collection on Postman though
+        # https://www.postman.com/postman/workspace/postman-public-workspace/request/12959542-4fff4089-9529-41c9-9ceb-58253e5cb342
+        result = requests.put(
+          f"{postman_base}/collections/{collection_id}/sync-with-schema-tasks",
+          headers=headers)
+
+        result.raise_for_status()
+
+        task_id, status = result.json()['taskId'], None
+
+        logger.info(f"Collection re-sync req., waiting for task: {task_id}")
+
+        while (True):
+            status = requests.get(url=f"{postman_base}/tasks/{task_id}",
+                                  headers=headers).json()['status']
+
+            if status != 'pending':
+                break
+
+            sleep(15)
+
+        logger.info(f"Task {task_id} status: {status}")
+
+
+logger = get_logger()
 
 
 if __name__ == "__main__":
@@ -76,4 +160,14 @@ if __name__ == "__main__":
         print(f"Usage: {sys.argv[0]} input-spec.json output-spec.json")
         sys.exit(1)
 
-    convert_spec(sys.argv[1], sys.argv[2])
+    try:
+        update_postman_collection(
+            patch_spec(sys.argv[1], sys.argv[2]),
+            api_id=getenv('POSTMAN_API_ID'),
+            schema_id=getenv('POSTMAN_SCHEMA_ID'),
+            file_path='openapi.json',
+            collection_id=getenv('POSTMAN_COLLECTION_ID'))
+
+    except Exception:
+        logging.exception("Exception raised!")
+        sys.exit(1)
