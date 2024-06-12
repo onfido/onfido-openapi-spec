ec2e4138bbd3efd3abf49e185bdd9af955966c2b
Davide Vacca <davide.vacca@onfido.com>
2024-06-12 16:18:55 +0200

    Update README.md files (incl. footer factorization) and fix run-prettier script

--- shell/run-prettier.sh
+++ shell/run-prettier.sh
@@ -11,7 +11,8 @@ else
 fi
 
 npm install prettier
-npx prettier $option openapi.yaml paths/*.yaml schemas/**/*.yaml responses/**/*.yaml \
-                     generators/**/config.yaml .github/workflows/*.yaml
-npx prettier $option --parser markdown generators/*/templates/README.mustache
+npx prettier $option openapi.yaml paths/*.yaml schemas/*/*.yaml responses/*.yaml \
+                     $(git ls-files generators/\*\*/config.yaml) .github/workflows/*.yaml
+npx prettier $option --parser markdown $(git ls-files generators/\*\*/templates/README.mustache) \
+                                       generators/common/templates/README_footer.mustache
 npx prettier $option --parser typescript generators/typescript-axios/templates/webhook-event-verifier.mustache
