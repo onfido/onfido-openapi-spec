897d02f81f9f9050538a69a76ce6830e3520cc7c
Davide Vacca <davide.vacca@onfido.com>
2024-06-18 10:33:34 +0200

    Add FileTransfer object to Node lib to allow in-memory only file upload/download

--- shell/run-prettier.sh
+++ shell/run-prettier.sh
@@ -15,4 +15,5 @@ npx prettier $option openapi.yaml paths/*.yaml schemas/*/*.yaml responses/*.yaml
                      $(git ls-files generators/\*\*/config.yaml) .github/workflows/*.yaml
 npx prettier $option --parser markdown $(git ls-files generators/\*\*/templates/README.mustache) \
                                        generators/common/templates/README_footer.mustache
-npx prettier $option --parser typescript generators/typescript-axios/templates/webhook-event-verifier.mustache
+npx prettier $option --parser typescript \
+                     generators/typescript-axios/templates/{webhook-event-verifier,file-transfer}.mustache
