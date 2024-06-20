e92d340b84690cce6851fa4a32530ac29911d75a
dvacca-onfido <134616519+dvacca-onfido@users.noreply.github.com>
2024-06-20 13:52:29 +0200

    [CAT-1241][Node] Add FileTransfer object to allow in-memory only file upload/download (#91)

--- shell/run-prettier.sh
+++ shell/run-prettier.sh
@@ -11,8 +11,9 @@ else
 fi
 
 npm install prettier
-npx prettier $option openapi.yaml paths/*.yaml schemas/*/*.yaml responses/*.yaml \
+npx prettier $option *.md openapi.yaml paths/*.yaml schemas/*/*.yaml responses/*.yaml \
                      $(git ls-files generators/\*\*/config.yaml) .github/workflows/*.yaml
 npx prettier $option --parser markdown $(git ls-files generators/\*\*/templates/README.mustache) \
                                        generators/common/templates/README_footer.mustache
-npx prettier $option --parser typescript generators/typescript-axios/templates/webhook-event-verifier.mustache
+npx prettier $option --parser typescript \
+                     generators/typescript-axios/templates/{webhook-event-verifier,file-transfer}.mustache
